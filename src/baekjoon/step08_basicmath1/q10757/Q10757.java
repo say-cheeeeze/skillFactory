fcf28a90951ae3d5e066f2800bb045c7ee70b0c2
cheeeeze <nyj9349@hanmail.net>
2023-06-07 20:59:52 +0900

    큰수 더하기 BigInteger

---
+++ src/baekjoon/step08_basicmath1/q10757/Q10757.java
@@ -0,0 +1,19 @@
+package baekjoon.step08_basicmath1.q10757;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Q10757 {
+
+	public static void main( String[] args ) throws IOException {
+		
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+		StringTokenizer st = new StringTokenizer( br.readLine() );
+		BigInteger num1 = new BigInteger( st.nextToken() );
+		BigInteger num2 = new BigInteger( st.nextToken() );
+		System.out.println( num1.add( num2 ) );
+	}
+}
