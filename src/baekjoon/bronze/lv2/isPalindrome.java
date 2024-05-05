20829512ccbd739a9f92c957858e66a9deb494b3
cheeeeze <nyj9349@hanmail.net>
2024-05-05 22:41:23 +0900

    [재귀] 펠린드롬

--- src/baekjoon/bronze/lv2/isPalindrome.java
+++ src/baekjoon/bronze/lv2/isPalindrome.java
@@ -1,5 +1,9 @@
 package baekjoon.bronze.lv2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 재귀의 귀재
  *
@@ -11,22 +15,19 @@ package baekjoon.bronze.lv2;
  * 팰린드롬이란, 앞에서부터 읽었을 때와 뒤에서부터 읽었을 때가 같은 문자열을 말한다. 팰린드롬의 예시로 AAA, ABBA, ABABA 등이 있고, 팰린드롬이 아닌 문자열의 예시로 ABCA, PALINDROME 등이 있다.
  *
  * 어떤 문자열이 팰린드롬인지 판별하는 문제는 재귀 함수를 이용해 쉽게 해결할 수 있다. 아래 코드의 isPalindrome 함수는 주어진 문자열이 팰린드롬이면 1, 팰린드롬이 아니면 0을 반환하는 함수다.
- * #include <stdio.h>
- * #include <string.h>
- *
- * int recursion(const char *s, int l, int r){
- *     if(l >= r) return 1;
- *     else if(s[l] != s[r]) return 0;
- *     else return recursion(s, l+1, r-1);
- * }
- *
- * int isPalindrome(const char *s){
- *     return recursion(s, 0, strlen(s)-1);
- * }
- *
- * int main(){
- *     printf("ABBA: %d\n", isPalindrome("ABBA")); // 1
- *     printf("ABC: %d\n", isPalindrome("ABC"));   // 0
+ * public class Main{
+ *     public static int recursion(String s, int l, int r){
+ *         if(l >= r) return 1;
+ *         else if(s.charAt(l) != s.charAt(r)) return 0;
+ *         else return recursion(s, l+1, r-1);
+ *     }
+ *     public static int isPalindrome(String s){
+ *         return recursion(s, 0, s.length()-1);
+ *     }
+ *     public static void main(String[] args){
+ *         System.out.println("ABBA: " + isPalindrome("ABBA"));
+ *         System.out.println("ABC: " + isPalindrome("ABC"));
+ *     }
  * }
  * 정휘는 위에 작성된 isPalindrome 함수를 이용하여 어떤 문자열이 팰린드롬인지 여부를 판단하려고 한다.
  *
@@ -67,7 +68,36 @@ package baekjoon.bronze.lv2;
  */
 public class isPalindrome {
 	
-	public static void main( String[] args ) {
+	public static void main( String[] args ) throws IOException {
+		
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+		int cnt = Integer.parseInt( br.readLine() );
+		
+		for( int i = 0; i < cnt; i++ ) {
+			
+			String word = br.readLine();
+			isPalindrome( word );
+		}
+		br.close();
+		
+	}
 	
+	public static int isPalindrome( String s ) {
+		int acc = 1;
+		return recursion( s, 0, s.length() - 1, acc );
+	}
+	
+	public static int recursion( String s, int l, int r, int acc ) {
+		if ( l >= r ) {
+			System.out.println( 1 + " " + acc );
+			return 1;
+		}
+		else if ( s.charAt( l ) != s.charAt( r ) ) {
+			System.out.println( 0 + " " + acc );
+			return 0;
+		}
+		else {
+			return recursion( s, l + 1, r - 1, ++acc );
+		}
 	}
 }
